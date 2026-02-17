0ab9a4a57c5b7de1bcfa59cf4f32d39d371a859c
Vitor Aleluia <vitoraleluia@outlook.pt>
2026-02-17 19:52:36 +0000

    Automatically generate types (not working yet)

--- OnTime.Client/vite.config.ts
+++ OnTime.Client/vite.config.ts
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1",
+        changeOrigin: true,
+        autoRewrite: true,
+      },
+    },
   },
 });
